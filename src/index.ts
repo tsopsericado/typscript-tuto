4501db1d5af77d6ec952ee83706a8635843f6a31
ricardo <ricardotsopse@gmail.com>
2023-09-30 15:12:20 +0100

    type array seen

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 //declaring variables in typescript
 //you can separate long numbers with underscore in typescript 
-let sales = 123_456_789;
-let course = "Typscript"
-let is_published  = true;
+// let sales = 123_456_789;
+// let course = "Typscript"
+// let is_published  = true;
 
 //built in type Javascript 
 
 // type any 
-let level;
+// let level;
 
-function render(document) {
-    console.log(document);
+// function render(document) {
+//     console.log(document);
     
-}
+// }
+
+//Type Arrays
+
+let numbers: number[] = []
+numbers.forEach( n => n.) 
