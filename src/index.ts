287d09bff5b2b01487cb65203008edcf979eb2f9
ricardo <ricardotsopse@gmail.com>
2023-09-30 18:14:12 +0100

    intersection type seen

--- src/index.ts
+++ src/index.ts
@@ -69,14 +69,30 @@
 //  };
 
  // Type union 
-  function kgToLbs(weight: number | string): number {
-    //Narrowing 
-    if (typeof weight === 'number') 
-     return  weight * 2.2;
-    else 
-    return parseInt(weight) * 2.2
+//   function kgToLbs(weight: number | string): number {
+//     //Narrowing 
+//     if (typeof weight === 'number') 
+//      return  weight * 2.2;
+//     else 
+//     return parseInt(weight) * 2.2
 
-  }
+//   }
 
- kgToLbs(10)
- kgToLbs('10')
+//  kgToLbs(10)
+//  kgToLbs('10')
+
+ // Type Intersection
+ type Draggable = {
+    drag: () => void
+ };
+
+ type Resizable = {
+    resize: () => void
+ };
+
+ type UIWidget = Draggable & Resizable;
+
+ let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+ }
