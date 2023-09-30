bdc1693263e5382a1c034a43dc773af34b9c4f09
ricardo <ricardotsopse@gmail.com>
2023-09-30 15:06:06 +0100

    type any seen

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//declaring variables in typescript
+//you can separate long numbers with underscore in typescript 
+let sales = 123_456_789;
+let course = "Typscript"
+let is_published  = true;
+
+//built in type Javascript 
+
+// type any 
+let level;
+
+function render(document) {
+    console.log(document);
+    
+}
